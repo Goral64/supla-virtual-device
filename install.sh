c8b26b8f8c742e7231e98a4bd4ff52259d586e18
Mariusz Górski <goral64@gmail.com>
2022-08-07 10:09:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,17 +10,17 @@ NC='\033[0m'
 echo "Getting the sources."
 
 if [ ! -d src ]; then
-  git clone https://github.com/Goral64/supla-core.git -q --single-branch --branch supla-mqtt-dev src >/dev/null || exit 1
+  git clone https://github.com/Goral64/supla-core.git -q --single-branch --branch supla-virtual-device src >/dev/null || exit 1
 fi
 
 (cd src && git pull >/dev/null && cd ..) || exit 1
 
 echo "Building. Be patient."
 
-(cd src/supla-dev/Release && make all >/dev/null 2>&1 && cd ../../..) || exit 1
+(cd src/supla-virtual-device/Release && make all >/dev/null 2>&1 && cd ../../..) || exit 1
 
 if [ ! -f supla-virtual-device ]; then
-  ln -s src/supla-dev/Release/supla-virtual-device supla-virtual-device
+  ln -s src/supla-virtual-device/Release/supla-virtual-device supla-virtual-device
 fi
 
 echo -e "${GREEN}OK!${NC}"
